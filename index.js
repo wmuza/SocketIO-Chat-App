a238fbc433968a4bd54a72c9389ed814da793e6c
wmuza <wmuza@meticulosity.co.za>
2018-07-11 21:12:12 +0200

    Upgrading to ES6

--- index.js
+++ index.js
@@ -10,65 +10,55 @@ app.get('/', (req, res) =>  {
 });
 
 // usernames which are currently connected to the chat
-var usernames = {};
+let usernames = {};
 
-function check_key(v)
-{
-	var val = '';
-	
-	for(var key in usernames)
-    {
-		if(usernames[key] == v)
-		val = key;
+const check_key = v =>{
+	let val = '';	
+	for(let key in usernames){
+		if(usernames[key] == v)	val = key;
 	}
 	return val;
 }
 
-io.on('connection', function (socket) {
-
+io.on('connection',  socket => {
 	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-		io.emit('updatechat', socket.username, data);
-	});
+	socket.on('sendchat', data => io.emit('updatechat', socket.username, data));
 
 	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username){
+	socket.on('adduser', username => {
 		// we store the username in the socket session for this client
 		socket.username = username;
 		// add the client's username to the global list
 		usernames[username] = socket.id;
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', socket.username + ' you have joined the chat');
+		socket.emit('updatechat', 'Chat Bot', socket.username + ' you have joined the chat');
 		// echo to client their username
 		socket.emit('store_username', username);
 		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		socket.broadcast.emit('updatechat', 'Chat Bot', username + ' has connected');
 		// update the list of users in chat, client-side
 		io.emit('updateusers', usernames);
 	});
 
 	// when the user disconnects.. perform this
-	socket.on('disconnect', function(){
+	socket.on('disconnect', () => {
 		// remove the username from global usernames list
 		delete usernames[socket.username];
 		// update list of users in chat, client-side
 		io.emit('updateusers', usernames);
 		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has left chat');
+		socket.broadcast.emit('updatechat', 'Chat Bot', socket.username + ' has left chat');
 	});
 	
 	// when the user sends a private msg to a user id, first find the username
-	socket.on('check_user', function(asker, id){
-		io.to(usernames[asker]).emit('msg_user_found', check_key(id));
-	});
+	socket.on('check_user', (asker, id) => io.to(usernames[asker]).emit('msg_user_found', check_key(id)));
 	
 	// when the user sends a private message to a user.. perform this
-	socket.on('msg_user', function(to_user, from_user, msg) {
+	socket.on('msg_user', (to_user, from_user, msg) => {
 		//emits 'msg_user_handle', this updates the chat body on client-side
 		io.to(usernames[to_user]).emit('msg_user_handle', from_user, msg);
 		//write the chat message to a txt file		
-		var wstream = fs.createWriteStream('chat_data.txt');		
+		const wstream = fs.createWriteStream('chat_data.txt');		
 		wstream.write(msg);
 		wstream.write('\r\n');
 		wstream.end();
@@ -78,7 +68,5 @@ io.on('connection', function (socket) {
 
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+http.listen(3000, () => console.log('listening on *:3000'));
     
